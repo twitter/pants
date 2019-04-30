ab04d17fa8f87fce941cc8c29a668353166698a0
Ity Kaul <ity@twitter.com>
2019-04-29 23:14:27 -0700

    Revert "use `type=` to register enum options (#7304)"
    This reverts commit ef043653edee4802ae0ade47fc4872ce5d142ef1.

--- src/python/pants/backend/jvm/tasks/jvm_compile/rsc/rsc_compile.py
+++ src/python/pants/backend/jvm/tasks/jvm_compile/rsc/rsc_compile.py
@@ -125,9 +125,9 @@ class _JvmCompileWorkflowType(enum(['zinc-only', 'rsc-then-zinc'])):
     if target.has_sources('.java') or \
       isinstance(target, JUnitTests) or \
       (isinstance(target, ScalaLibrary) and tuple(target.java_sources)):
-      target_type = _JvmCompileWorkflowType.zinc_only
+      target_type = _JvmCompileWorkflowType('zinc-only')
     elif target.has_sources('.scala'):
-      target_type = _JvmCompileWorkflowType.rsc_then_zinc
+      target_type = _JvmCompileWorkflowType('rsc-then-zinc')
     else:
       target_type = None
     return target_type
